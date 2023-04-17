1e2d1cf51edea76aa45c776684287f1d6dd21099
Silaev Kirill <s-kir@yandex.ru>
2023-04-17 18:55:20 +0300

    Added task 151

---
+++ src/main/java/HomeWork1/HomeWork1.java
@@ -0,0 +1,20 @@
+package HomeWork1;
+
+import java.util.Scanner;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String str = console.nextLine();
+        System.out.println(reverseWords(str));
+    }
+
+    public static String reverseWords(String s) {
+            String reversedString = "";
+            String[] words = s.trim().split("\\s+");
+            for (int i = words.length - 1; i > 0; --i) {
+                reversedString += words[i] + " ";
+            }
+            return reversedString + words[0];
+    }
+}
