59822dc20a5db93844a8a80d3012df5aed53e79b
Silaev Kirill <s-kir@yandex.ru>
2023-05-02 18:40:39 +0300

    Добавил задачу 1 из Д/З 5

---
+++ src/main/java/HomeWork5/Task1.java
@@ -0,0 +1,29 @@
+//1) Подсчитать количество искомого слова, через map (наполнением значение, где искомое слово будет являться ключом), вносить все слова не нужно
+//        Пример:
+//        Россия идет вперед всей планеты. Планета — это не Россия.
+//        Запрос: Россия
+//        Ответ: Россия - 2
+
+package HomeWork5;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task1 {
+    public static void main(String[] args) {
+        String text = "Россия идет вперед всей планеты. Планета — это не Россия.";
+        countWords(text);
+    }
+    public static void countWords(String str) {
+        str = str.replaceAll("[^а-яА-я\\s]", "").replaceAll("\\s{2,}", " ");
+        String[] words = str.split(" ");
+        Map<String, Integer> mapWords = new HashMap<>();
+        for (String s : words) {
+            mapWords.putIfAbsent(s, 0);
+            mapWords.put(s, mapWords.get(s) + 1);
+        }
+        for (Map.Entry<String, Integer> items : mapWords.entrySet()) {
+            System.out.printf("%s - %d раз(а)%n", items.getKey(), items.getValue());
+        }
+    }
+}
